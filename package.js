58d96ecea048ae8eebdc755ed57c08feaca57cc2
Pierre Wieser <pwieser@trychlos.org>
2023-01-31 19:10:39 +0100

    Remove hardcoded npm dependencies from package.js

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     name: 'pwix:bootbox',
-    version: '1.0.1',
-    summary: 'A Bootstrap-based alert() and confirm(), so a client-only Meteor package',
+    version: '1.1.0',
+    summary: 'Bootstrap-based alert() and confirm() dialogs, so a client-only Meteor package',
     git: 'https://github.com/trychlos/pwix-bootbox',
     documentation: 'README.md'
 });
@@ -12,6 +12,7 @@ Package.onUse( function( api ){
         'pwixBootbox'
     ]);
     api.mainModule( 'src/client/js/index.js', 'client' );
+    api.mainModule( 'src/server/js/index.js', 'server' );
 });
 
 Package.onTest( function( api ){
@@ -24,12 +25,11 @@ Package.onTest( function( api ){
 function configure( api ){
     api.versionsFrom( '2.9.0' );
     api.use( 'blaze-html-templates@2.0.0', 'client' );
-    api.use( 'ecmascript', 'client' );
+    api.use( 'ecmascript' );
     api.use( 'less@4.0.0', 'client' );
-    api.use( 'pwix:i18n@1.0.0', 'client' );
+    api.use( 'pwix:i18n@1.0.0' );
+    api.use( 'tmeasday:check-npm-versions' );
 }
 
-Npm.depends({
-    'bootstrap': '5.2.1',
-    'jquery-ui-dist': '1.13.2'
-});
+// NPM dependencies are checked in /src/server/js/check_npms.js
+// See also https://guide.meteor.com/writing-atmosphere-packages.html#npm-dependencies
