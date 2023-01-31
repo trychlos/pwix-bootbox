4cfe3d260cb511720f55b6fc0789878eb77413f0
Pierre Wieser <pwieser@trychlos.org>
2023-01-31 19:30:04 +0100

    Post-release minimal version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'pwix:bootbox',
-    version: '1.1.0',
+    version: '1.1.0-vnext',
     summary: 'Bootstrap-based alert() and confirm() dialogs, so a client-only Meteor package',
     git: 'https://github.com/trychlos/pwix-bootbox',
     documentation: 'README.md'
