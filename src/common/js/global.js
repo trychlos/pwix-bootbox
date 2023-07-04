fe521c0cc6909db1a78fb69c021d2cf52640681c
Pierre Wieser <pwieser@trychlos.org>
2023-07-04 19:16:15 +0200

    Minor code refactoring

--- src/common/js/global.js
+++ src/common/js/global.js
@@ -2,9 +2,4 @@
  * pwix:bootbox/src/common/js/global.js
  */
 
-//console.log( 'pwix:bootbox/src/common/js/config.js defining globally exported Bootbox object' );
-
-Bootbox = {
-    // internationalization
-    i18n: {}
-};
+Bootbox = {};
